095236bbe134d829b695d71b957d731544b3ce80
Ekanuel <ekanumaf@gmail.com>
2022-10-04 12:27:43 +0300

    Create bag2.c

---
+++ bag2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// void print_msg( void );
+float do_it(char arg1, char arg2, char arg3);
+float do_it(char arg1, char arg2, char arg3){
+//does something
+}
+main(){
+    // void print_msq( void )
+void print_a_number(int arg1);
+    puts("This is a message to print");
+    return 0;
+}
